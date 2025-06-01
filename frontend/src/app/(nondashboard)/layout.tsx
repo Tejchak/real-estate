3cfae7ad2bbdeeeffac3fca96e283a1f5ff6a3f6
Tejchak <chakravarthy.t@northeastern.edu>
2025-06-01 14:57:00 +0530

    Initial commit: Real Estate project setup

---
+++ frontend/src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,16 @@
+
+import Navbar from '@/components/navbar'
+import React from 'react'
+
+const Layout = ({ children}: {children: React.ReactNode }) => {
+  return (
+    <div>
+      <Navbar />
+      <main className={'h-full flex w-full flex-col pt-[${NAVBAR_HEIGHT}px]'}>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
