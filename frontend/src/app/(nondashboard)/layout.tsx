4c6600c5cf78ccd9d483b685ae48c71e37102067
Tejchak <chakravarthy.t@northeastern.edu>
2025-06-11 16:59:27 +0530

    finished authentication for navigation and different tenant/manager specific pages

--- frontend/src/app/(nondashboard)/layout.tsx
+++ frontend/src/app/(nondashboard)/layout.tsx
@@ -1,11 +1,35 @@
 "use client";
 import Navbar from '@/components/Navbar'
 import { useGetAuthUserQuery } from '@/state/api';
-import React from 'react'
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 const Layout = ({ children}: {children: React.ReactNode }) => {
-  const {data: authUser } = useGetAuthUserQuery();
-  console.log("authUser:", authUser);
+  const {data: authUser, isLoading: authLoading} = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        userRole === 'manager' && pathname.startsWith("/search") ||
+        userRole === 'manager' && pathname ==="/")
+        {
+          router.push(
+            "/mangers/properties",
+            {scroll: false}
+          );
+        } else {
+          setIsLoading(false);
+        }
+    }
+  }, [authUser, router, pathname]);
+
+  if (authLoading || isLoading) return <>Loading... </>; 
   return (
     <div>
       <Navbar />
